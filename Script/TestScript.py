f91da982e1f43e71d4c20c7d5c58880843ab5bf5
Raihan Kirana <raihankirana@gmail.com>
2025-04-07 21:40:30 +0700

    add another files

---
+++ Script/TestScript.py
@@ -0,0 +1,44 @@
+
+
+
+class Mobil: 
+
+    def __init__(self , nama , tipe): 
+        self.nama = nama 
+        self.tipe = tipe 
+
+    def run(self):
+
+        print("kuciing {self.nama} berlalri ")
+
+
+
+
+
+
+
+
+# def main():
+
+#     test = MO
+#     # CLUE: create object from Assistant Class here
+#     pass
+
+
+# if __name__ == "__main__":
+#     main()
+
+
+# def readFile() :
+#     f = open("receiver_list.txt")
+#     print(f.readline())
+#     print(f.readline())
+#     f.close()
+
+
+
+# readFile()
+
+
+
+# test = lambda x , 
